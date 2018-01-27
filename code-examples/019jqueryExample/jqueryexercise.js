5da158d2bef6bf9ac9608977b78543781ca51440
Emily <emily.p.sanford@gmail.com>
2018-01-26 22:28:38 -0600

    Create jqueryexercise.js

---
+++ code-examples/019jqueryExample/jqueryexercise.js
@@ -0,0 +1,11 @@
+$("div").css("background", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+$("div:first-of-type").css("color", "pink");
+
+$("img").attr({
+  src: "http://4everstatic.com/pictures/674xX/animals/wildlife/capybara,-bird-246671.jpg",
+  title: "Adult Capybara"
+});
+
+$("input").attr("type", "password");
