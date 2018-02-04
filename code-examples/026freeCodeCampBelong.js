f537c8d6531ae519c0b528cd853e367563320989
Emily <emily.p.sanford@gmail.com>
2018-02-03 22:00:47 -0600

    Update and rename 026freeCodeCampBelong to 026freeCodeCampBelong.js

--- code-examples/026freeCodeCampBelong.js
+++ code-examples/026freeCodeCampBelong.js
@@ -1,3 +1,5 @@
+// CHALLENGE
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
 
 function getIndexToIns(arr, num) {
   var lowestIndex;
